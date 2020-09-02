dd03125127755d3e1532da60ca834dfd941043c5
PawanXccelerata <pawan@xccelerata.com>
2020-09-02 13:40:24 -0400

    RTCPeerConnection Object implemented

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express = require('express');
+var express = require('express.io');
 var app = express();
-console.log('server started');
+app.http().io();
+var PORT = 3000;
+console.log('server started on port' + PORT);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -8,4 +10,27 @@ app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
-app.listen(3200);
+app.io.route('ready', function(req) {
+    req.io.join(req.data.chat_room);
+    req.io.join(req.data.signal_room);
+    app.io.room(req.data).broadcast('announce', {
+        message: 'New Client in the' +
+            req.data + 'room'
+    })
+})
+
+app.io.route('send', function(req) {
+    app.io.room(req.data.room).broadcast('message', {
+        message: req.data.message,
+        author: req.data.author
+    });
+})
+
+app.io.route('signal', function(req) {
+    app.io.room(req.data.room).broadcast('signaling_message', {
+        type: req.data.type,
+        message: req.data.message
+    });
+})
+
+app.listen(PORT);
